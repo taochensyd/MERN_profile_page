cdb85347951d08cf1c784e3df2ab224535874b34
Tao Chen <54517105+taochensyd@users.noreply.github.com>
2022-04-21 20:35:45 +1000

    add dotenv file

--- profile_page_api/index.js
+++ profile_page_api/index.js
@@ -1,25 +1,24 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import profileRoutes from './routes/profileDataRoutes.js';
+import express from "express";
+import bodyParser from "body-parser";
+import profileRoutes from "./routes/profileDataRoutes.js";
+import "dotenv/config";
 
 const app = express();
 
 // port 3000 for React, used port 5000 for express
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // will use JSON data in the application
 app.use(bodyParser.json());
 
 // users endpoint
-app.use('/profile', profileRoutes);
+app.use("/profile", profileRoutes);
 
-app.get('/', (req, res) => {
-    // console.log("GET request");
-    res.send("HOME GET request");
+app.get("/", (req, res) => {
+  // console.log("GET request");
+  res.send("HOME GET request");
 });
-
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
-
